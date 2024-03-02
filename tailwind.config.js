585122a934c62d2f24365764b2874a241b1ff168
Shifalee Khan <78851167+Shifalee@users.noreply.github.com>
2024-03-02 10:32:55 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.html"],
+    theme: {
+      extend: {
+        fontFamily: {
+          robo: ['Roboto'],
+          popi: ['Poppins']
+        },
+        colors: {
+          primary: "#541554",
+          secondary: "#F4EDE4",
+          primaryDark: "#4a154b",
+        }
+      },
+    },
+    plugins: [],
+  }
+  
+  
